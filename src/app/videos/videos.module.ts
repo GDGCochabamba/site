a48d237604d1f107b55b58ebb11107f19a65c7ef
Julio Silva <julio.sguz@gmail.com>
2018-02-24 11:11:38 -0400

    Add base modules

---
+++ src/app/videos/videos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VideosRoutingModule } from './videos-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    VideosRoutingModule
+  ],
+  declarations: []
+})
+export class VideosModule { }
