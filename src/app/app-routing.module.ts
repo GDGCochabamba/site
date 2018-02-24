38caad752c1a39cca33b6bf1b1e80f61058ca3f5
Julio Silva <julio.sguz@gmail.com>
2018-02-24 11:19:44 -0400

    Add routes on routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './home/home.module#HomeModule'
+  },
+  {
+    path: '',
+    loadChildren: './events/events.module#EventModule'
+  },
+  {
+    path: '',
+    loadChildren: './videos/videos.module#VideosModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
